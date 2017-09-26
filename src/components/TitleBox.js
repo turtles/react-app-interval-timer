5eed4e8573731ab68ef0906307b8b478770ebec3
Leah Sandacz <sandas@outlook.com>
2017-09-25 20:02:14 -0700

    Link to GitHub

--- src/components/TitleBox.js
+++ src/components/TitleBox.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react'
-import {Grid, PageHeader, small} from 'react-bootstrap'
+import {Grid, Row,Col,Button,ButtonToolbar,PageHeader} from 'react-bootstrap'
 
 class TitleBox extends React.Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.openGitHub = this.openGitHub.bind(this);
+  }
+  openGitHub()
+  {
+    var page = window.open("https://github.com/turtles/react-app-interval-timer", '_blank');
+    page.focus();
+  }
   render() {
+
     return (
       <Grid>
-        <PageHeader>Interval Timer <small>{"Timer that play sounds on an interval to remind you it's running."}</small></PageHeader>
+        <PageHeader>
+          Interval Timer <small>{"Timer that play sounds on an interval to remind you it's running."}</small> <Button type="button" pullRight="true" onClick={this.openGitHub}>View Source</Button>
+        </PageHeader>
       </Grid>
     );
   }
